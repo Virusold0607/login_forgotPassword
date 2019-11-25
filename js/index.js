a0d8e472004e2085874050bdee73917690e4b4ac
virusold <virusold0607@gmail.com>
2019-11-25 20:10:15 +0530

    changes in background and JS added

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function backgroundChange(){
+    var image_container = ['background-image.jpg','bg2.jpg','bg3.jpg','bg4.jpg','bg5.jpg','bg6.jpg','bg7.jpg','bg8.jpg'];
+    
+    var random_number = Math.floor(Math.random() * 8);
+    
+    var body_element = document.body.style;
+    /*setting css properties*/ 
+    body_element.backgroundImage = 'url(images/' + image_container[random_number] + ')';
+    body_element.backgroundRepeat = 'no-repeat';
+    body_element.backgroundPosition = 'center';
+    body_element.backgroundSize = 'cover';
+    
+}
+backgroundChange();
