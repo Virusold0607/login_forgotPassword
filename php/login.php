11b6834496c6a63987148621fadd4e8ae73e4e1f
virusold <virusold0607@gmail.com>
2019-11-26 20:08:22 +0530

    login.php updated

--- php/login.php
+++ php/login.php
@@ -1,3 +1,46 @@
 <?php
-echo "login form";
+
+class Login{
+    public $connection;
+    public $username;
+    public $password;
+    function __construct($connection,$username,$password){
+        $this->connection = $connection;
+        $this->username = $username;
+        $this->password = $password;
+    }
+
+    function login(){
+        $query = "SELECT username, password FROM user WHERE username = '".$this->username."' AND  password = '".$this->password."'";
+        $content = mysqli_query($this->connection,$query) or die(mysqli_error());
+        $row = mysqli_fetch_assoc($content);
+
+        if($row['username'] == $this->username && $row['password'] == $this->password){
+            echo "Welcome $this->username!";
+        }
+        else{
+            echo "username or password is incorrect!";
+        }
+        
+    }
+}
+$connection = mysqli_connect("localhost",'root','','users');
+
+$username = '';
+$password = '';
+
+if(!empty($_POST['username']) && !empty($_POST['password'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    /*MySQL injection prevention*/
+    $username = stripslashes($username);
+    $password = stripslashes($password);
+
+    $username = mysqli_escape_string($connection,$username);
+    $password = mysqli_escape_string($connection,$password);
+    /*****/
+}
+$login_form = new Login($connection,$username,$password);
+$login_form->login();
 ?>
